69070ce8c3c41d2d6a17ef14e619219eb472eb51
James Arnott <james@rno.tt>
2025-04-07 12:39:21 +0100

    chore: removed router

--- webview-ui/src/index.tsx
+++ webview-ui/src/index.tsx
@@ -2,11 +2,11 @@ import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 
 import "./index.css"
-import Router from "./router"
 import "../../node_modules/@vscode/codicons/dist/codicon.css"
+import App from "./App"
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
-		<Router />
+		<App />
 	</StrictMode>,
 )
